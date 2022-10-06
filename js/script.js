28737197bcb5723408e4fd9710fcb35b512ca836
Nathanael <dev@nathabonfim59.com>
2022-10-05 21:56:44 -0300

    feat: exibição em colunas e experiencias com indices

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+var qtdExperiencias = 1;
 
 function addExperiencia(){
     var divExp = document.getElementById("experiencia");
     var exp = document.createElement("input");
+
+    qtdExperiencias++;
+
     exp.type = "text";
     exp.setAttribute("class", "form-control mt-2"); 
-    exp.setAttribute("name", "exp2"); 
+    exp.setAttribute("name", "exp[" + qtdExperiencias + "]"); 
 
     divExp.appendChild(exp);
 }
