6fb44ee46926dba064787e09fb3805472739211f
vitorgibim <vitor.gibim@edu.unipar.br>
2022-10-06 21:27:22 -0300

    ADD objetivo e melhorias

--- pages/index.php
+++ pages/index.php
@@ -11,6 +11,7 @@ $curriculo->nome = trim($_POST['nome']);
 $curriculo->nascimento = $_POST['nascimento'];
 $curriculo->celular = trim($_POST['celular']);
 $curriculo->email = trim($_POST['email']);
+$curriculo->objetivo = $_POST['objetivo'];
 $curriculo->experiencias = $_POST['exp'];
 $curriculo->idade = calcularIdade($curriculo->nascimento);
 
@@ -48,27 +49,41 @@ function renderizarExperiencia($experiencia) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="/style/imprimir.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        
     </head>
     <body>
         <main class="container">
-            <div class="sobre">
-                
-                <h1><?php echo $curriculo->nome; ?></h1>
-                <h1><?php echo $curriculo->celular; ?></h1>
-                <h1><?php echo $curriculo->email; ?></h1>
+            <div class="sobre" >
+
+                <h1><b><u><?php echo $curriculo->nome; ?></u></b></h1>
+                <p><b><?=$curriculo->idade?> Anos</b></p>
+
+                <p><b><u>CONTATO</u></b></p>
+                <h2><?php echo $curriculo->celular; ?></h2>
+                <h2><?php echo $curriculo->email; ?></h2>
             </div>
             <div class="experiencias">
-            <?php
+                <p><b><u>EXPERIÊNCIAS</u></b></p>
+                <?php
             if (isset($_POST['nome'])){
                 foreach ($curriculo->experiencias as $index => $experiencia) {
+                    
                     renderizarExperiencia($experiencia);
+                    // echo('<hr>');
                 }
-
+                
             }
             ?>
-            <div class="flex mb-6 bg-gray-400 p-3 rounded align-middle">
-                <button class="ml-auto mt-2 btn bg-indigo-600 hover:bg-indigo-800 text-white" type="button" id="imprimir" onclick = "imprimir()" >Imprimir currículo</button>
-            </div>
+            <p><b><u>OBJETIVO</u></b></p>
+            <h2><?php echo $curriculo->objetivo; ?></h2>
+
+                <div class="flex mb-6 p-3 rounded align-middle">
+                    <button class="mr-auto mt-2 btn bg-indigo-600 hover:bg-indigo-800 text-white" type="button" id="imprimir" onclick = "imprimir()" >Imprimir currículo</button>
+                </div>
+            
             </div>
         </main>
     </body>
